3071cfe8ef65c10f2a26bf27907590acf78b6f08
Angeleyko <angeleyko@i.ua>
2024-08-13 17:45:45 +0300

    first

---
+++ my-form/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  
+  return (
+    <div className="App">
+      <h1>ФОРМА</h1>
+      <form>
+      <div>
+        <input type="email"/>
+      </div>
+      <br/>
+      <button type="submit">Надіслати</button>
+      </form>
+      
+    </div>
+  );
+}
+
+export default App;
